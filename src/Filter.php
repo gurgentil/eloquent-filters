2a17ad11091b0dfed83df603467958ce12542bb3
Gustavo Gentil <gustavorgentil@outlook.com>
2020-12-06 20:26:43 -0300

    docs: describe apply method

--- src/Filter.php
+++ src/Filter.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 
 interface Filter
 {
+    /**
+     * Apply filter.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     * @return Builder
+     */
     public function apply(Builder $builder, $value);
 }
